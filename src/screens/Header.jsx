ae8e588075b7a61e8666199a43eb47007f2c6164
Fuuma <masa81hiro69@gmail.com>
2023-05-04 17:46:10 +0900

    [add]Header仮

---
+++ src/screens/Header.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import LogoImg from "../assets/img/zanpy-logo.png";
+import { useNavigate } from "react-router-dom";
+import "../index.css";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  return (
+    <header className='h-24 bg-main_green shadow-xl flex items-center justify-between pl-10 pr-14 py-2'>
+      {/* ロゴ */}
+      <div className='flex items-center'>
+        <img src={LogoImg} alt='logo' className='h-16' />
+      </div>
+      {/* 遷移ボタン */}
+      <div className='flex space-x-8 mr-auto pt-3'>
+        <button className='font-arial text-test font-black text-xl ml-24'>
+          担当エリア
+        </button>
+        <button className='font-arial text-test font-black text-xl'>
+          検索
+        </button>
+        <button className='font-arial text-test font-black text-xl'>
+          脱出掲示板
+        </button>
+      </div>
+      {/* ログアウトボタン */}
+      <div className='flex bg-white rounded-2xl items-center'>
+        <button
+          className='font-arial text-test font-black text-xl px-3 py-3'
+          onClick={() => navigate("/ログイン")}
+        >
+          ログアウト
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         test: "#706F6F",
         loginBotton: "#84ED82",
         main_green: "#A0E99E",
+        sub_color: "#D9F8D9",
       },
     },
   },
