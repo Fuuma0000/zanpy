87dde05d504b491c54c1c2497a94d307d6074910
Fuuma <masa81hiro69@gmail.com>
2023-05-05 21:05:30 +0900

    [fix]横幅を200に設定

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,73 +2,69 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 
 const SearchBox = () => {
   return (
     <div className='fixed bg-white left-16 top-72 rounded-3xl shadow-xl'>
       {/* 検索ボックス */}
-      <Typography component='div'>
-        <div className='flex flex-col gap-8 p-12 '>
-          <Autocomplete
-            disablePortal
-            options={types}
-            sx={{ fontWeight: 900 }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label='種類'
-                InputLabelProps={{
-                  style: {
-                    color: "#706F6F",
-                    fontWeight: 900,
-                  },
-                }}
-              />
-            )}
-          />
-          <TextField
-            id='outlined-basic'
-            label='名前'
-            variant='outlined'
-            //font-weightをboldにする
-            // sx={{ fontWeight: 900 }} これではならなかった
-            InputLabelProps={{
-              style: {
-                color: "#706F6F",
-                fontWeight: 900,
-              },
-            }}
-          />
-          <Autocomplete
-            disablePortal
-            options={status}
-            sx={{ width: 300 }}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label='ステータス'
-                InputLabelProps={{
-                  style: {
-                    color: "#706F6F",
-                    fontWeight: 900,
-                  },
-                }}
-              />
-            )}
-          />
+      <div className='flex flex-col gap-8 p-12 '>
+        <Autocomplete
+          disablePortal
+          options={types}
+          sx={{ width: 200 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='種類'
+              InputLabelProps={{
+                style: {
+                  color: "#706F6F",
+                  fontWeight: 900,
+                },
+              }}
+            />
+          )}
+        />
+        <TextField
+          id='outlined-basic'
+          label='名前'
+          sx={{ width: 200 }}
+          variant='outlined'
+          InputLabelProps={{
+            style: {
+              color: "#706F6F",
+              fontWeight: 900,
+            },
+          }}
+        />
+        <Autocomplete
+          disablePortal
+          options={status}
+          sx={{ width: 200 }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label='ステータス'
+              InputLabelProps={{
+                style: {
+                  color: "#706F6F",
+                  fontWeight: 900,
+                },
+              }}
+            />
+          )}
+        />
 
-          <div className='flex justify-center'>
-            <Button
-              variant='contained'
-              style={{ backgroundColor: "#A0E99E" }}
-              className='w-32'
-            >
-              検索
-            </Button>
-          </div>
+        <div className='flex justify-center'>
+          <Button
+            variant='contained'
+            style={{ backgroundColor: "#A0E99E" }}
+            className='w-32'
+          >
+            検索
+          </Button>
         </div>
-      </Typography>
+      </div>
     </div>
   );
 };
