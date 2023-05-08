4c462c4bb1bc250176320d471a204fdb7f3d8544
Fuuma <masa81hiro69@gmail.com>
2023-05-09 01:07:07 +0900

    [add]

---
+++ src/screens/MemoScreen.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import SearchBox from "../components/SearchBox";
+import MyAreaTable from "../components/MyAreaTable";
+
+const MemoScreen = () => {
+  return (
+    <>
+      <Header />
+      <SearchBox />
+      <MyAreaTable />
+    </>
+  );
+};
+
+export default MemoScreen;
