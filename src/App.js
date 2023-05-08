5de7c1d6fdff783aca13d644ae1ee1a1b059a546
Fuuma <masa81hiro69@gmail.com>
2023-05-09 01:52:35 +0900

    [hotfix]RecoilRootの追加

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import MyAreaScreen from "./screens/MyAreaScreen";
 import MemoScreen from "./screens/MemoScreen";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={`/my-area`} element={<MyAreaScreen />} />
-        <Route path={`/login`} element={<LoginScreen />} />
-        <Route path={`/memo`} element={<MemoScreen />} />
-        <Route path={`/*`} element={<LoginScreen />} />
-      </Routes>
+      <RecoilRoot>
+        <Routes>
+          <Route path={`/my-area`} element={<MyAreaScreen />} />
+          <Route path={`/login`} element={<LoginScreen />} />
+          <Route path={`/memo`} element={<MemoScreen />} />
+          <Route path={`/*`} element={<LoginScreen />} />
+        </Routes>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
