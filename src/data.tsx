3edb464f7b983ed045b6ccec3d397ad1315470e2
Fuuma <masa81hiro69@gmail.com>
2023-05-08 19:30:19 +0900

    [fix]脱走にした

--- src/data.tsx
+++ src/data.tsx
@@ -7,6 +7,6 @@ export interface ColourOption {
 export const colourOptions: readonly ColourOption[] = [
   { value: "0", label: "無事", color: "#5DFF59" },
   { value: "1", label: "怪我あり", color: "#FF6161" },
-  { value: "2", label: "脱走中", color: "#FBFF49" },
+  { value: "2", label: "脱走", color: "#FBFF49" },
   { value: "3", label: "不明", color: "#D9D9D9" },
 ];
