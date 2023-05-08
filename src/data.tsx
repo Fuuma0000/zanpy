42cc902b2d58d9deffd18281e82cba30b3a298bf
Fuuma <masa81hiro69@gmail.com>
2023-05-08 19:29:57 +0900

    [fix]迷子保護を消した。色を明るめにした

--- src/data.tsx
+++ src/data.tsx
@@ -5,9 +5,8 @@ export interface ColourOption {
 }
 
 export const colourOptions: readonly ColourOption[] = [
-  { value: "0", label: "怪我あり", color: "#FFCCCC" },
-  { value: "1", label: "脱走中", color: "#FEFFB6" },
-  { value: "2", label: "迷子保護", color: "#B9DCEF" },
-  { value: "3", label: "無事", color: "#C0FBBF" },
-  { value: "4", label: "不明", color: "#D9D9D9" },
+  { value: "0", label: "無事", color: "#5DFF59" },
+  { value: "1", label: "怪我あり", color: "#FF6161" },
+  { value: "2", label: "脱走中", color: "#FBFF49" },
+  { value: "3", label: "不明", color: "#D9D9D9" },
 ];
