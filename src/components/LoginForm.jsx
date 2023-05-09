6b58f2f77beb915e529d3076ea1debde65eaddaf
Fuuma <masa81hiro69@gmail.com>
2023-05-10 03:07:52 +0900

    [change]パスワードの入力を見えないように変更変更

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -34,6 +34,7 @@ const LoginForm = () => {
             </label>
             <input
               name='password'
+              type='password'
               className='w-full rounded border bg-form px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring'
             />
           </div>
