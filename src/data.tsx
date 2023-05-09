097a03296b31760ee898492eeb3f5ce5477863aa
Fuuma <masa81hiro69@gmail.com>
2023-05-09 11:05:09 +0900

    [change]怪我あり→怪我

--- src/data.tsx
+++ src/data.tsx
@@ -6,7 +6,7 @@ export interface ColourOption {
 
 export const colourOptions: readonly ColourOption[] = [
   { value: "0", label: "無事", color: "#5DFF59" },
-  { value: "1", label: "怪我あり", color: "#FF6161" },
+  { value: "1", label: "怪我", color: "#FF6161" },
   { value: "2", label: "脱走", color: "#FBFF49" },
   { value: "3", label: "不明", color: "#D9D9D9" },
 ];
