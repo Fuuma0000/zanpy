5e773095754a3e9fad1186a631ce2fa58b41a855
Fuuma <masa81hiro69@gmail.com>
2023-05-09 11:13:22 +0900

    [change]色薄くしよた

--- src/data.tsx
+++ src/data.tsx
@@ -5,8 +5,8 @@ export interface ColourOption {
 }
 
 export const colourOptions: readonly ColourOption[] = [
-  { value: "0", label: "無事", color: "#5DFF59" },
-  { value: "1", label: "怪我", color: "#FF6161" },
-  { value: "2", label: "脱走", color: "#FBFF49" },
+  { value: "0", label: "無事", color: "#96F094" },
+  { value: "1", label: "怪我", color: "#EF9C9C" },
+  { value: "2", label: "脱走", color: "#FDFF89" },
   { value: "3", label: "不明", color: "#D9D9D9" },
 ];
