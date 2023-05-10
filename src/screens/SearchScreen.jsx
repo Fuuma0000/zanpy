bc41304b0dcb6e192b7c99f65c1a2e48ae9049af
Fuuma <masa81hiro69@gmail.com>
2023-05-10 13:34:59 +0900

    [update]

--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import SearchBox from "../components/SearchBox";
 import { useSetRecoilState } from "recoil";
 import { myAreaOpenState } from "../atoms/MyAreaOpenState";
+import MyAreaTable from "../components/MyAreaTable";
 
 const SearchScreen = () => {
   const setMyAreaOpen = useSetRecoilState(myAreaOpenState);
@@ -12,10 +13,10 @@ const SearchScreen = () => {
   return (
     <>
       <Header></Header>
-      {/* TODO:やる */}
       {/* 検索ボックス */}
       <SearchBox></SearchBox>
       {/* 検索結果テーブル */}
+      <MyAreaTable></MyAreaTable>
     </>
   );
 };
