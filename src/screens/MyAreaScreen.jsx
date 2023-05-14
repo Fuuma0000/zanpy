4fb7d1ca76e7631846426506acb364e214791e03
Fuuma <masa81hiro69@gmail.com>
2023-05-15 06:46:07 +0900

    [add]画面遷移時の検索条件のリセット機能を追加

--- src/screens/MyAreaScreen.jsx
+++ src/screens/MyAreaScreen.jsx
@@ -1,13 +1,26 @@
 import Header from "./Header";
 import SearchBox from "../components/SearchBox";
 import StatesTable from "../components/StatesTable";
-import { useSetRecoilState } from "recoil";
+import { useSetRecoilState, useRecoilState } from "recoil";
 import { myAreaOpenState } from "../atoms/MyAreaOpenState";
+import { selectedTypeState } from "../atoms/SelectedTypeState";
+import { selectedAreaState } from "../atoms/SelectedAreaState";
+import { selectedNameState } from "../atoms/SelectedNameState";
+import { selectedStatusState } from "../atoms/SelectedStatusState";
 
 const MyAreaScreen = () => {
   const setMyAreaOpen = useSetRecoilState(myAreaOpenState);
-  //初回レンダリング時にmyAreaOpenStateをtrueにする
+  const setSelectedType = useSetRecoilState(selectedTypeState);
+  const setSelectedArea = useSetRecoilState(selectedAreaState);
+  const setSelectedName = useSetRecoilState(selectedNameState);
+  const setSelectedStatus = useSetRecoilState(selectedStatusState);
+  //レンダリング時にmyAreaOpenStateをtrueにする
   setMyAreaOpen(true);
+  //レンダリング時に検索条件をリセットする
+  setSelectedType("");
+  setSelectedArea("");
+  setSelectedName("");
+  setSelectedStatus("");
 
   // /api/myareaにGETリクエストを送る
 
--- src/screens/SearchScreen.jsx
+++ src/screens/SearchScreen.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import Header from "./Header";
 import SearchBox from "../components/SearchBox";
-import { useSetRecoilState } from "recoil";
+import { useSetRecoilState, useRecoilState } from "recoil";
 import { myAreaOpenState } from "../atoms/MyAreaOpenState";
 import StatesTable from "../components/StatesTable";
+import { selectedTypeState } from "../atoms/SelectedTypeState";
+import { selectedAreaState } from "../atoms/SelectedAreaState";
+import { selectedNameState } from "../atoms/SelectedNameState";
+import { selectedStatusState } from "../atoms/SelectedStatusState";
 // import axios from "axios";
 // import { useEffect } from "react";
 
 const SearchScreen = () => {
   const setMyAreaOpen = useSetRecoilState(myAreaOpenState);
+  const setSelectedType = useSetRecoilState(selectedTypeState);
+  const setSelectedArea = useSetRecoilState(selectedAreaState);
+  const setSelectedName = useSetRecoilState(selectedNameState);
+  const setSelectedStatus = useSetRecoilState(selectedStatusState);
   //初回レンダリング時にmyAreaOpenStateをfalseにする
   setMyAreaOpen(false);
+  //レンダリング時に検索条件をリセットする
+  setSelectedType("");
+  setSelectedArea("");
+  setSelectedName("");
+  setSelectedStatus("");
 
   // function createData(area, name, area, type, states, memos, memoTime) {
   //   return { area, name, area, type, states, memos, memoTime };
